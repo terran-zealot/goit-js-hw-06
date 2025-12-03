4bb45fe87a14de72ddf1219c3215bb2d06d22d3f
terran-zealot <vikinglaurasun@gmail.com>
2025-12-03 15:37:55 +0200

    homework js 06 first

---
+++ js/task-02.js
@@ -0,0 +1,29 @@
+class Storage {
+    constructor(params) {
+        this.item = params;
+    }
+
+    getItems() {
+        return this.item;
+    }
+
+    addItem(newItem) {
+        this.item.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.item = this.item.filter(item => item !== itemToRemove);
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
